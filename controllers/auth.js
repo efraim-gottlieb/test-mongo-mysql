ba71ed82d6927bea266a6db55ec532a684a8ab80
EfraimGottlieb <efgo583208979@gmail.com>
2026-01-01 11:55:04 +0200

    adde readme file

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,3 @@
-// import { ObjectId } from "bson";
-
 export async function createUser(req, res) {
   if (!(req.body.username && req.body.password)) {
     res.json({ error: "body error" });
@@ -24,22 +22,3 @@ export async function createUser(req, res) {
     }
   }
 }
-
-// export async function getProducts(req, res) {
-//   const conn = req.mongoDbConn;
-//   const collection = await conn.collection("products");
-//   const products = await collection.find().toArray();
-//   res.json({ message: products });
-// }
-
-// export async function getProductById(req, res) {
-//   const id = req.params.id;
-//   const conn = req.mongoDbConn;
-//   const collection = await conn.collection("products");
-//   try {
-//     const products = await collection.findOne({ _id: new ObjectId(id) });
-//     res.json({ message: products });
-//   } catch (error) {
-//     res.status(404).json({ error: "not found" });
-//   }
-// }
