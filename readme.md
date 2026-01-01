b7abbb5f4b8ec8b6d527a4003db1f4f37f09185b
EfraimGottlieb <efgo583208979@gmail.com>
2026-01-01 11:36:33 +0200

    adde readme file

---
+++ readme.md
@@ -0,0 +1,8 @@
+run docker compode up -d
+npm install
+
+to run usuing nodemon (dev):
+npm run dev 
+
+to run usuing node (prodaction):
+npm run start
--- routes/messages.js
+++ routes/messages.js
@@ -3,6 +3,7 @@ import * as messagesController from "../controllers/messages.js";
 const router = express.Router();
 
 router.route("/")
+  .get(messagesController.getAllMessages)
   .post(messagesController.createMessage)
 
 export default router;
